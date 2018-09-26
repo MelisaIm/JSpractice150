1eea28d2ef2bdbcc52b29d93805c7fc29561427f
Melisa Im <mim@adobe.com>
2018-09-26 14:16:54 -0700

    Print document content

---
+++ ex2.js
@@ -0,0 +1,14 @@
+// Write a JavaScript program to print the contents of the current window.
+
+function printScreen() {
+  window.print();
+}
+
+/*
+REVIEW
+- Had to google to get started, did not fully understand the task
+- Exercise required some HTML to use the function in the context of an actual page
+
+RESOURCES
+https://developer.mozilla.org/en-US/docs/Web/API/Window/print
+*/
