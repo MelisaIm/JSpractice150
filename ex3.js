bd1c547432c337598c73e5df781afe6926389818
Melisa Im <mim@adobe.com>
2018-09-26 15:06:45 -0700

    Add comments to ex3

--- ex3.js
+++ ex3.js
@@ -22,7 +22,13 @@ console.log(
 
 /*
 REVIEW
-
+- The solution checks if dd or mm is less than 10, which makes sense, 
+my solution checks for strict length which is the same but potentially
+more flexible
+- Overall happy with my solution, initially I thought of using an array for
+the months, but all I had to do was add 1 to the value
 
 RESOURCES
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/getDate
+https://www.w3resource.com/javascript-exercises/javascript-basic-exercise-3.php
 */
