e2b842955b5303f54f723e112565b72e49b05048
Melisa Im <mim@adobe.com>
2018-09-26 14:07:14 -0700

    Code review

--- ex1.js
+++ ex1.js
@@ -5,7 +5,9 @@ const today = new Date(Date.now());
 const day = dayOfWeek(today.getDay());
 
 console.log(`Today is ${day}`);
-console.log(`Current time is: ${today.getHours()}:${today.getMinutes()}`);
+console.log(
+  `Current time is: ${today.getHours()}:${today.getMinutes()}:${today.getSeconds()}`
+);
 
 function dayOfWeek(num) {
   switch (num) {
@@ -27,3 +29,16 @@ function dayOfWeek(num) {
       return "ERROR";
   }
 }
+
+/*
+REVIEW:
+https://www.w3resource.com/javascript-exercises/javascript-basic-exercise-1.php
+- Could use array index instead of switch case
+- There is a .getSeconds() method
+- The logic in the solution goes a step further to show "Noon" or "Midnight" in standard 12hr clock
+whereas my solution just uses the military standard time 24hr clock
+- My critique of the solution is that the time formatting is ugly 10PM : 30 : 38 should be 10:30:38 PM
+
+RESOURCES USED:
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/getHours
+*/
